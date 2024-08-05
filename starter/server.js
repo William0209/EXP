e18dc9a83960f61c3302c3e6104cce46d958f983
William0209 <janssenwilliam02@gmail.com>
2024-08-05 14:04:27 +0200

    ENV Variables

--- starter/server.js
+++ starter/server.js
@@ -1,8 +1,11 @@
-const app = require("./app");
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 
-const port = 3000;
+const app = require('./app');
+
+// console.log(process.env);
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(
-    `App running on port: ${port}`
-  );
+  console.log(`App running on port: ${port}`);
 });
