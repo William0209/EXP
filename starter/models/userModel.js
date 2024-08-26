f0aa716039b6fa560ffdaca71a3e2f16c24990cd
William0209 <janssenwilliam02@gmail.com>
2024-08-26 20:27:26 +0200

    chore: Update user model with required fields and validation

---
+++ starter/models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
